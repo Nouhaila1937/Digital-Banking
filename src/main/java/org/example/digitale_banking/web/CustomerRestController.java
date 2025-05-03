b50b1faca4b3741693d76c44b40b5ac563d19f6f
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-05-03 23:22:16 +0100

    sécurité

--- src/main/java/org/example/digitale_banking/web/CustomerRestController.java
+++ src/main/java/org/example/digitale_banking/web/CustomerRestController.java
@@ -7,6 +7,7 @@ import org.example.digitale_banking.Dtos.CustomerDTO;
 import org.example.digitale_banking.entities.Customer;
 import org.example.digitale_banking.exceptions.CustomerNotFoundException;
 import org.example.digitale_banking.service.BankAccountService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,26 +20,33 @@ public class CustomerRestController {
     private BankAccountService bankAccountService;
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAnyAuthority('SCOPE_USER')")
     public List <CustomerDTO> getCustomers() {
         return bankAccountService.listCustomers();
     }
 
+
     @GetMapping("/customer/{id}")
+    @PreAuthorize("hasAnyAuthority('SCOPE_USER')")
     public CustomerDTO getCustomer(@PathVariable(name="id") Long id) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(id);
     }
 
+    @PreAuthorize("hasAnyAuthority('SCOPE_ADMIN')")
     @PostMapping("/addcustomer")
     public CustomerDTO addCustomer(@RequestBody CustomerDTO customerDTO) {
         return bankAccountService.saveCustomer(customerDTO);
     }
 
+    @PreAuthorize("hasAnyAuthority('SCOPE_ADMIN')")
     @PutMapping("/updatecustomer/{id}")
     public CustomerDTO updateCustomer(@PathVariable Long id ,@RequestBody CustomerDTO customerDTO) {
         customerDTO.setId(id);
         return bankAccountService.updateCustomer(customerDTO);
 
     }
+
+    @PreAuthorize("hasAnyAuthority('SCOPE_ADMIN')")
     @DeleteMapping("/customers/{id}")
     public void deleteCustomer(@PathVariable Long id){
         bankAccountService.deleteCustomer(id);
